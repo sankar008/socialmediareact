d64fb1c56a0fc44b7485c9c5b33dbcde59a2ff9a
Manas Sahoo <manas_sahoo@outlook.com>
2022-06-13 23:07:27 +0530

    Remove react-fade-in

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import FadeIn from "react-fade-in";
 import { Navbar } from "../../components/Navbar/NavbarLanding";
 import { IconSection } from "./IconSection/Index";
 import { LandingStart } from "./landingStart/index";
@@ -7,15 +6,13 @@ import { LandingStart } from "./landingStart/index";
 export default function Landing() {
   return (
     <>
-      <FadeIn>
-        <div className="is-white">
-          <Navbar />
-          <div className="landing-wrapper">
-            <LandingStart />
-            <IconSection />
-          </div>
+      <div className="is-white">
+        <Navbar />
+        <div className="landing-wrapper">
+          <LandingStart />
+          <IconSection />
         </div>
-      </FadeIn>
+      </div>
     </>
   );
 }
